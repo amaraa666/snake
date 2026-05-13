c07a0977cb88c24c32380091636b59123aa447b8
admin <admin@admins-MacBook-Pro.local>
2026-05-13 17:04:52 +0800

    feature: make a game winable and can lose

--- src/biydaalt/ShapeDrawing.java
+++ src/biydaalt/ShapeDrawing.java
@@ -1,24 +1,41 @@
 package biydaalt;
 
 import java.awt.*;
-import java.util.ArrayList;
+import java.awt.Toolkit;
 
+import java.util.ArrayList;
 import javax.swing.*;
 
 public class ShapeDrawing extends JComponent {
+    snake SnakeWindow;
+	int randomX;
+	int randomY;
 	ArrayList<Integer> myXArr = new ArrayList<Integer>();
 	ArrayList<Integer> myYArr = new ArrayList<Integer>();
-	int myFruitX = 25;
-	int myFruitY = 50;
+	int myFruitX;
+	int myFruitY;
+	int GoalScore=5;
 	int myScore = 1;
 	int moveDis = 25;
 	String Direction="up";
 	String BefDirection="down";
+    
+    public ShapeDrawing(snake parent) {
+    	SnakeWindow = parent;
+    }
 
-	public void paint(Graphics g)
-    {
+	public void paint(Graphics g){
+        Graphics2D g4 = (Graphics2D) g;        
         Graphics2D g1 = (Graphics2D) g;        
-        Graphics2D g2 = (Graphics2D) g;        
+        Graphics2D g2 = (Graphics2D) g;     
+        Graphics2D g3 = (Graphics2D) g;   
+        
+        g4.setColor(Color.black);
+        g4.fillRect(0,40,500,360);
+
+        g3.setColor(Color.red);
+ 	    g3.drawOval(myFruitX, myFruitY, 20, 20);
+  	    g3.fillOval(myFruitX, myFruitY, 20, 20);
         for(int i = 0; i < myScore ;i++) {
         	
         	if(i == 0) 
@@ -30,33 +47,33 @@ public class ShapeDrawing extends JComponent {
             	g2.setColor(Color.pink);
             	g2.drawRect(myXArr.get(i), myYArr.get(i), 20, 20);
                 g2.fillRect(myXArr.get(i), myYArr.get(i), 20, 20);
-        	}
-        	
+        	}	
         } 
-        Graphics2D g3 = (Graphics2D) g;        
-        g3.setColor(Color.red);
- 	    g3.drawOval(myFruitX, myFruitY, 20, 20);
-  	    g3.fillOval(myFruitX, myFruitY, 20, 20);
     }
-    
-    public void Touch() {
-    	
-    	double randomX = 1;
-    	double randomY = 1;
-    	while((randomY%25 !=0 || randomX%25!=0) || myXArr.contains(randomX) || myYArr.contains(randomY)) 
+   
+	public void AutoPosGenerate() {
+		randomX = 1;
+		randomY = 1;
+    	while((randomY%50 !=0 || randomX%25!=0)  || ( myXArr.contains(randomX) || myYArr.contains(randomY)) || (randomX == 0 || randomY == 0) ) 
     	{
     		
-			randomX = Math.round(Math.random()*320/25)*25;
-	    	randomY = Math.round(Math.random()*320/25)*25;
+			randomX = (int) Math.round(Math.random()*450/25)*25;
+	    	randomY = (int) Math.round(Math.random()*350/50)*50;
     	}
+	}
+	//jimsend hureh uyd jims random baidlaar garah
+    public void ChangeFruitPos() {
+    	
+    	    AutoPosGenerate();
         	this.myFruitX = (int) randomX;
         	this.myFruitY = (int) randomY;       	
     }
      
-    
+    //jims ideh uyd suul nemegdeh
     public void IsTouched() {
     		if(myXArr.getFirst() == this.myFruitX && myYArr.getFirst() == this.myFruitY) {
-    			Touch();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+    	        Toolkit.getDefaultToolkit().beep();
+    			ChangeFruitPos();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
     			if(Direction.equals("left")) {
     				myXArr.add(myXArr.getLast()+25);
         			myYArr.add(myYArr.getLast());	
@@ -76,42 +93,82 @@ public class ShapeDrawing extends JComponent {
     				myXArr.add(myXArr.getLast());
         			myYArr.add(myYArr.getLast()-25);
     			}
-    			myScore+=1;    			
+    			
+    		
+    			myScore+=1;    	
+    			SnakeWindow.ScoreCounter();
+    			if(GoalScore == myScore) {
+    				System.out.println("WON");
+    				SnakeWindow.timer.stop();
+    				JOptionPane.showMessageDialog(null, "Won the game! Score: " + myScore);
+    				ResetGame();
+    			}
     		}
     }
-	
-    public void MoveShapeUp() {
-    		if(myYArr.getLast() >= 25) {
-    			if(!BefDirection.equals("down")) {
-    				myYArr.addFirst(myYArr.get(0)-moveDis);
-        			myYArr.removeLast();
-        			myXArr.addFirst(myXArr.get(0));
-        			myXArr.removeLast();	
-    			}else {
-    				MoveShapeDown();
+    
+    //game over
+    public boolean IsGameOver(int x, int y) {
+    		for(int i = 0 ; i < myXArr.size(); i++) {
+    			if(myXArr.get(i) == x && myYArr.get(i) == y) {
+    				System.out.println("Game over");
+    				SnakeWindow.timer.stop();
+    				JOptionPane.showMessageDialog(null, "Game Over! Score: " + myScore);
+    				ResetGame();
+    				return true;
     			}
+    		}
+    		return false;
+    }
+    
+    //reset game
+    public void ResetGame() {
+	    AutoPosGenerate();
+    	this.myXArr.removeAll(myXArr);
+    	myXArr.add(randomX);
+    	this.myYArr.removeAll(myYArr);
+    	myYArr.add(randomY);
+	    AutoPosGenerate();
+    	this.myFruitX = randomX;
+    	this.myFruitY = randomY;
+    	this.myScore = 1;
+		SnakeWindow.ScoreCounter();
+    }
+    
+    //movement sections
+	int x,y;
+    public void MoveShapeUp() {
+    		if(myYArr.getFirst() >= 75) {
+    				x = myXArr.get(0);
+    				y = myYArr.get(0)-moveDis;
+    				
+    				if(!IsGameOver(x,y)) {
+    					myYArr.addFirst(y);
+            			myYArr.removeLast();
+            			myXArr.addFirst(x);
+            			myXArr.removeLast();
+    				}
+
     		}else {
-    			myYArr.addFirst(325);
+    			myYArr.addFirst(375);
     			myYArr.removeLast();
     			myXArr.addFirst(myXArr.get(0));
     			myXArr.removeLast();
     		}
     		this.repaint();
     }
-    
 
 	public void MoveShapeDown() {
-    		if(myYArr.getLast() <= 325) {
-    			if(!BefDirection.equals("up")) {
-    				myYArr.addFirst(myYArr.get(0)+moveDis);
-        			myYArr.removeLast();
-        			myXArr.addFirst(myXArr.get(0));
-        			myXArr.removeLast();
-    			}else {
-    				MoveShapeUp();
-    			}
+    		if(myYArr.getFirst() <= 350) {
+    				x = myXArr.get(0);
+    				y = myYArr.get(0)+moveDis;
+    				if(!IsGameOver(x,y)) {
+        				myYArr.addFirst(y);
+            			myYArr.removeLast();
+            			myXArr.addFirst(x);
+            			myXArr.removeLast();
+    				}
     		}else {
-    			myYArr.addFirst(25);
+    			myYArr.addFirst(50);
     			myYArr.removeLast();
     			myXArr.addFirst(myXArr.get(0));
     			myXArr.removeLast();
@@ -120,17 +177,17 @@ public class ShapeDrawing extends JComponent {
     }
     
     public void MoveShapeLeft() {
-    		if(myXArr.getLast() >= 25) {
-    			if(!BefDirection.equals("right")) {
-    				myXArr.addFirst(myXArr.get(0)-moveDis);
-        			myXArr.removeLast();
-        			myYArr.addFirst(myYArr.get(0));
-        			myYArr.removeLast();	
-    			}else {
-    				MoveShapeRight();
-    			}
+    		if(myXArr.getFirst() >= 25) {
+    				x = myXArr.get(0)-moveDis;
+    				y = myYArr.get(0);
+    				if(!IsGameOver(x,y)) {
+    					myXArr.addFirst(x);
+            			myXArr.removeLast();
+            			myYArr.addFirst(y);
+            			myYArr.removeLast();	
+    				}		
     		}else {
-    			myXArr.addFirst(325);
+    			myXArr.addFirst(475);
     			myXArr.removeLast();
     			myYArr.addFirst(myYArr.get(0));
     			myYArr.removeLast();
@@ -139,21 +196,21 @@ public class ShapeDrawing extends JComponent {
     }
     
     public void MoveShapeRight() {
-    		if(myXArr.getLast() <= 325) {
-    			if(!BefDirection.equals("left")) {
-    				myXArr.addFirst(myXArr.get(0)+moveDis);
-        			myXArr.removeLast();
-        			myYArr.addFirst(myYArr.get(0));
-        			myYArr.removeLast();	
-    			}else {
-    				MoveShapeLeft();
-    			}
+    		if(myXArr.getFirst() <= 450) {
+    				x = myXArr.get(0)+moveDis;
+    				y = myYArr.get(0);
+    				if(!IsGameOver(x,y)) {
+    					myXArr.addFirst(x);
+            			myXArr.removeLast();
+            			myYArr.addFirst(y);
+            			myYArr.removeLast();	
+    				}
     		}else {
-    			myXArr.addFirst(25);
+    			myXArr.addFirst(0);
     			myXArr.removeLast();
     			myYArr.addFirst(myYArr.get(0));
     			myYArr.removeLast();
     		} 	
     		this.repaint();
-    	} 
+    } 
 }
--- src/biydaalt/snake.java
+++ src/biydaalt/snake.java
@@ -9,64 +9,130 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class snake implements ActionListener , KeyListener {
-    static ShapeDrawing mySnake = new ShapeDrawing();
-	Timer timer;
+    ShapeDrawing mySnake;
+	Timer timer = new Timer(150,this);
 	String myEvent;
-	static JFrame frame = new JFrame("Snake game");
-	static ShapeDrawing myDraw = new ShapeDrawing();
-	static JPanel panel = new JPanel();
-	static snake myKeyListener = new snake(); // Create the instance
-    static JButton myBtn = new JButton("Stop");
-    static JLabel myLabel = new JLabel("Score:"+mySnake.myScore);
+	 JFrame frame = new JFrame("Snake game");
+	 JPanel panel = new JPanel();
+     JButton myBtn = new JButton("Stop");
+     JButton myStartBtn = new JButton("Start");
+     Integer[] myLvl = {5,10,15};
+     JComboBox<Integer> comboBox = new JComboBox<>(myLvl);
+     JLabel myLabel = new JLabel();
+     JLabel myLabelComb = new JLabel("Хожих нөхцөл: ");
+
+	
+	public snake() {
+		mySnake = new ShapeDrawing(this);
+		mySnake.AutoPosGenerate();
+		mySnake.myXArr.add(mySnake.randomX);
+		mySnake.myYArr.add(mySnake.randomY);
+		mySnake.AutoPosGenerate();
+		mySnake.myFruitX = mySnake.randomX;
+		mySnake.myFruitY = mySnake.randomY;
+	        frame.setSize(500, 500);
+			myLabel.setBounds(10, 5, 100, 30);
+			myBtn.setFocusable(false);
+			myStartBtn.setFocusable(false);
+			myLabel.setFocusable(false);	
+			myBtn.setBounds(260, 410, 100, 20);
+			myStartBtn.setBounds(380, 410, 100, 20);
+			myLabelComb.setBounds(80, 5, 130, 30);
+			comboBox.setBounds(200, 5, 100, 30);
+			comboBox.setFocusable(false);
+			myBtn.addActionListener(this);
+			myStartBtn.addActionListener(this);
+	        comboBox.addActionListener(this);
+			myLabel.setText("Score:"+mySnake.myScore);
+			
+			frame.add(myLabelComb);
+	        frame.add(comboBox);
+			frame.add(myLabel);
+			frame.add(myBtn);
+			frame.add(myStartBtn);
+	        frame.add(mySnake);
+	        frame.setFocusable(true);
+	        frame.addKeyListener(this);
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    	frame.setAutoRequestFocus(true);
+	        frame.setVisible(true);
+	}
 	@Override
     public void keyPressed(KeyEvent e) {
-		timer = new Timer(200 , this);
-		timer.start();
-		myEvent = KeyEvent.getKeyText(e.getKeyCode());
-		mySnake.BefDirection = mySnake.BefDirection.equals(mySnake.Direction) ? mySnake.BefDirection :mySnake.Direction;  
-		mySnake.Direction = myEvent.toLowerCase();
+		myEvent = KeyEvent.getKeyText(e.getKeyCode()).toLowerCase();
+		//System.out.println(myEvent);
+
+		if(myEvent.equals("↑")) {
+			myEvent = "up";
+		}
+		else if(myEvent.equals("←")){
+			myEvent = "left";
+		}
+		else if(myEvent.equals("→")){
+			myEvent = "right";
+		}
+		else if(myEvent.equals("↓")){
+			myEvent = "down";
+		}else {
+			myEvent = mySnake.Direction;
+		}
+		
+		if(mySnake.myXArr.size()==1)
+		{
+			mySnake.Direction = myEvent;
+		}else {
+			if(mySnake.Direction.equals("up") && myEvent.equals("down")) {
+				mySnake.Direction = "up";
+			}
+			else if(mySnake.Direction.equals("down") && myEvent.equals("up")) {
+				mySnake.Direction = "down";
+			}
+			else if(mySnake.Direction.equals("left") && myEvent.equals("right")) {
+				mySnake.Direction = "left";
+			}
+			else if(mySnake.Direction.equals("right") && myEvent.equals("left")) {
+				mySnake.Direction = "right";
+			}else if (!mySnake.Direction.equals(myEvent)){
+				mySnake.Direction = myEvent;
+			}
+		}
     }
 	
     @Override
-    public void keyTyped(KeyEvent e) {}
+    public void keyTyped(KeyEvent e) {}	
     @Override
-    public void keyReleased(KeyEvent e) {}
+    public void keyReleased(KeyEvent e) {
+    }
 	
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() instanceof JButton) {
+		if (e.getSource()==this.myBtn) {
 	        System.out.println("The game is stopped!!");
 	        timer.stop();
 	    }else if (e.getSource() == timer) {
-	    		mySnake.IsTouched();
-	    		myLabel.setText("Score:"+mySnake.myScore);
-	    		if(myEvent.equals("Up")) {
+	    		if(mySnake.Direction.equals("up")) {
 	    			mySnake.MoveShapeUp();
-	    		}else if(myEvent.equals("Down")) {
+	    		}else if(mySnake.Direction.equals("down")) {
 	    			mySnake.MoveShapeDown();
-	    		}else if(myEvent.equals("Right")) {
+	    		}else if(mySnake.Direction.equals("right")) {
 	    			mySnake.MoveShapeRight();
-	    		}else if(myEvent.equals("Left")) {
+	    		}else if(mySnake.Direction.equals("left")) {
 	    			mySnake.MoveShapeLeft();
-	    		}	    
-	    	}
+	    		}
+	    		mySnake.IsTouched();
+	    }else if(e.getSource() == this.myStartBtn){
+	    	timer.start();
+	    }else if(e.getSource() == this.comboBox) {
+	    	int selected = (int) comboBox.getSelectedItem();
+	    	mySnake.GoalScore = selected;
+	    	this.frame.setAutoRequestFocus(true);
+	    }
+	}
+	
+	public void ScoreCounter() {
+		myLabel.setText("Score:"+mySnake.myScore);
 	}
 
 	public static void main(String[] args) {
-		mySnake.myXArr.add(100);
-		mySnake.myYArr.add(150);
-	        frame.setSize(500, 500);
-			myLabel.setBounds(10, 20, 100, 30);
-			myBtn.setFocusable(false);
-			myLabel.setFocusable(false);
-			myBtn.setBounds(50, 400, 100, 20);
-			myBtn.addActionListener(myKeyListener);
-			frame.add(myLabel);
-			frame.add(myBtn);
-	        frame.add(mySnake);
-	        frame.setFocusable(true);
-	        frame.addKeyListener(myKeyListener);
-	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        frame.setVisible(true);
+		new snake();
 	}
-
 }
